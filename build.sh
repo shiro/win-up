f8deefe2f330ea44ea2bcb6902d83943773029fa
shiro <shiro@usagi.io>
2019-10-05 23:08:54 +0200

    Improve build

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
-mkdir -p output &2>/dev/null
+rm -r output 2>/dev/null
+mkdir -p output 2>/dev/null
 
 # only x64 as of a dependency limitation
-compile/Ahk2Exe.exe /in winUp.ahk /out output/winUp.exe /bin "compile/Unicode 64-bit.bin"
+compile/Ahk2Exe.exe /in win-up.ahk /out output/win-up.exe /bin "compile/Unicode 64-bit.bin"
 
-cp *.dll output
+cp lib/*.dll output
+
+cd output
+tar -cvzf win-up.tar.gz win-up.exe *.dll
